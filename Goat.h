6ebf4844345b16ebf9981616187fd419ba7960c5
Your Name <brightsy04@gmail.com>
2026-04-03 01:25:00 -0700

    commit5

--- Goat.h
+++ Goat.h
@@ -10,6 +10,7 @@ private:
     string name;
     int age;
     string color;
+
 public: 
     Goat()                          { name = ""; age = 0; color = ""; }
     // write three more constructors
@@ -20,13 +21,19 @@ public:
     // setters and getters
     void set_name(string n)         { name = n; };
     string get_name() const         { return name; };
+
     void set_age(int a)             { age = a; };
     int get_age() const             { return age; }
+
     void set_color(string c)        { color = c; }
     string get_color() const        { return color; }
 
     // write overloaded < operator for the std::list
         bool operator<(const Goat& other) const{
+        if (name != other.name)
+            return name < other.name;
+        if (age != other.age)
+            return age < other.age;
         return name < other.name ;
     }
 };
